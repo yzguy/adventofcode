2d0a198bdb5f0a8a96713f559c63eb5c3fce7aa7
Adam Smith <zero1three@gmail.com>
2020-12-09 07:07:36 -0800

    Initial 2020/Day9

---
+++ 2020/day9/test_day9.py
@@ -0,0 +1,21 @@
+#
+#
+#
+
+from part1 import part_one_solution
+from part2 import part_two_solution
+import unittest
+
+
+class TestSolutions(unittest.TestCase):
+    entries = [
+    ]
+
+    def test_part_one_solutions(self):
+        self.assertEqual(part_one_solution(self.entries), 0)
+
+    def test_part_two_solutions(self):
+        self.assertEqual(part_two_solution(self.entries), 0)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ 2020/day9/util.py
@@ -0,0 +1,9 @@
+#
+#
+#
+
+def read_file_to_list(filename):
+    with open(filename) as reader:
+        lines = reader.read().splitlines()
+
+    return lines
