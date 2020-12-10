ed0176e4f32dc670438edc0d337e82db30f2a1b6
Adam Smith <zero1three@gmail.com>
2020-12-10 10:25:14 -0800

    Add 2020/Day10/Part1

--- 2020/day10/part1.py
+++ 2020/day10/part1.py
@@ -3,4 +3,21 @@
 #
 
 def part_one_solution(entries):
-    return 0
+    entries.sort()
+
+    # Add device adapter joltage
+    entries.append(entries[-1] + 3)
+
+    # Keep track of diffs of 1 and 3
+    differences = {
+        1: 0,
+        3: 0
+    }
+
+    current_joltage_output = 0
+    for entry in entries:
+        diff = entry - current_joltage_output
+        differences[diff] += 1
+        current_joltage_output = entry
+
+    return differences[1] * differences[3]
