59a594ec384861c22c106b01b9f6cb17944f3145
Adam Smith <zero1three@gmail.com>
2022-12-05 12:36:46 -0500

    2022/day4/tests

--- 2022/day4/test.py
+++ 2022/day4/test.py
@@ -2,7 +2,7 @@
 
 from part1 import part_one_solution
 from part2 import part_two_solution
-from util import read_file_to_list
+from util import read_file_to_list, derange
 import unittest
 
 PART_ONE_ANSWER = 2
@@ -18,6 +18,23 @@ class TestSolutions(unittest.TestCase):
     def test_part_two_solution(self):
         self.assertEqual(part_two_solution(self.entries), PART_TWO_ANSWER)
 
+class TestUtil(unittest.TestCase):
+
+    def test_derange(self):
+        cases = [
+            "13-16",
+            "20-25",
+            "3-4",
+        ]
+
+        results = [
+            list(range(13, 17)),
+            list(range(20, 26)),
+            list(range(3, 5)),
+        ]
+
+        for idx, case in enumerate(cases):
+            self.assertEqual(results[idx], derange(case))
 
 if __name__ == "__main__":
     unittest.main()
