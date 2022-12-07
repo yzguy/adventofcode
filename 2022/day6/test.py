6c1781efc624f3867eff060c2b2612b1ff1d6657
Adam Smith <zero1three@gmail.com>
2022-12-06 21:40:51 -0500

    2022/day6/tests

--- 2022/day6/test.py
+++ 2022/day6/test.py
@@ -2,7 +2,7 @@
 
 from part1 import part_one_solution
 from part2 import part_two_solution
-from util import read_file_to_list
+from util import read_file_to_list, unique
 import unittest
 
 PART_ONE_ANSWER = 7
@@ -17,6 +17,18 @@ class TestSolutions(unittest.TestCase):
 
     def test_part_two_solution(self):
         self.assertEqual(part_two_solution(self.entries), PART_TWO_ANSWER)
+
+
+class TestUtil(unittest.TestCase):
+
+    def test_unique(self):
+        cases = [
+            [1, 1, 2, 3],
+            [1, 2, 3, 4],
+        ]
+
+        self.assertEqual(False, unique(cases[0]))
+        self.assertEqual(True, unique(cases[1]))
 
 
 if __name__ == "__main__":
